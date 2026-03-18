f71a051f8de322f3c6181ad6b749e54a0873095d
Matvey <romankovmatvej59@gmail.com>
2026-03-19 03:13:05 +0700

    Add IScheduleService

---
+++ CollegeSchedule_Romankov/Services/IScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using CollegeSchedule_Romankov.DTO;
+
+namespace CollegeSchedule_Romankov.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime startDate, DateTime endDate);
+    }
+}
