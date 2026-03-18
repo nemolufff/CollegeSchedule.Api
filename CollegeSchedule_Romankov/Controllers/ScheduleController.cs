775b8963c7318b8ac9b21e4f9a9dd2fc20f4c0cd
Matvey <romankovmatvej59@gmail.com>
2026-03-19 03:35:09 +0700

    Add ScheduleController

---
+++ CollegeSchedule_Romankov/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using CollegeSchedule_Romankov.Data;
+using CollegeSchedule_Romankov.Models;
+using CollegeSchedule_Romankov.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeSchedule_Romankov.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+
+            return Ok(result);
+        }
+    }
+}
