a812dd907f92456f8c8a5146406df584abf8c4dc
Matvey <romankovmatvej59@gmail.com>
2026-03-19 02:05:15 +0700

    Add LessonTime.cs

--- CollegeSchedule_Romankov/Models/LessonTime.cs
+++ CollegeSchedule_Romankov/Models/LessonTime.cs
@@ -1,6 +1,22 @@
-﻿namespace CollegeSchedule_Romankov.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CollegeSchedule_Romankov.Models
 {
+    [Table("lesson_time")]
     public class LessonTime
     {
+        [Key]
+        [Column("lesson_time_id")]
+        public int LessonTimeId { get; set; }
+
+        [Column("lesson_number")]
+        public int LessonNumber { get; set; }
+
+        [Column("time_start")]
+        public TimeOnly TimeStart { get; set; }
+
+        [Column("time_end")]
+        public TimeOnly TimeEnd { get; set; }
     }
 }
