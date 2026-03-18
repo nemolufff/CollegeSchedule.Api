0c3f1164066c68c19bf2d54b829444f522deecb8
Matvey <romankovmatvej59@gmail.com>
2026-03-19 03:07:10 +0700

    Add DTO classes

---
+++ CollegeSchedule_Romankov/DTO/LessonDto.cs
@@ -0,0 +1,17 @@
+﻿using CollegeSchedule_Romankov.Models;
+
+namespace CollegeSchedule_Romankov.DTO
+{
+    public class LessonDto
+    {
+        public int LessonNumber { get; set; }
+        public string Time { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Teacher { get; set; } = null!;
+        public string TeacherPosition { get; set; } = null!;
+        public string Classroom { get; set; } = null!;
+        public string Building { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public Dictionary<LessonGroupPart, LessonPartDto?> GroupParts { get; set; } = new();
+    }
+}
---
+++ CollegeSchedule_Romankov/DTO/ScheduleByDateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CollegeSchedule_Romankov.DTO
+{
+    public class ScheduleByDateDto
+    {
+        public DateTime LessonDate { get; set; }
+        public string Weekday { get; set; } = null!;
+        public List<LessonDto> Lessons { get; set; } = new();
+    }
+}
