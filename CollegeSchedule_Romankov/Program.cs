dab4448ca0552463efa8ee1d22199914af5dfd6d
Matvey <romankovmatvej59@gmail.com>
2026-03-19 02:54:28 +0700

    Update Program.cs

--- CollegeSchedule_Romankov/Program.cs
+++ CollegeSchedule_Romankov/Program.cs
@@ -1,6 +1,23 @@
+using CollegeSchedule_Romankov.Data;
+using CollegeSchedule_Romankov.Middlewares;
+using CollegeSchedule_Romankov.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotNetEnv.Env.Load();
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                       $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                       $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,8 +34,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
