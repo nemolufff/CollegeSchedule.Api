1cd5aba2822fdcc2e1b939ada622abe7ed006342
Matvey <romankovmatvej59@gmail.com>
2026-03-19 02:07:53 +0700

    Add Specialty.cs

--- CollegeSchedule_Romankov/Models/Specialty.cs
+++ CollegeSchedule_Romankov/Models/Specialty.cs
@@ -1,6 +1,17 @@
-﻿namespace CollegeSchedule_Romankov.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CollegeSchedule_Romankov.Models
 {
+    [Table("specialties")]
     public class Specialty
     {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
     }
 }
