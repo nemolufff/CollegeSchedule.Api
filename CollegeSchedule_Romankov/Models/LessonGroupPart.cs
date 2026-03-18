8a1b68d0ce82b00f762a8b236099530baab4e902
Matvey <romankovmatvej59@gmail.com>
2026-03-19 02:02:32 +0700

    Add LessonGroupPart.cs

--- CollegeSchedule_Romankov/Models/LessonGroupPart.cs
+++ CollegeSchedule_Romankov/Models/LessonGroupPart.cs
@@ -1,6 +1,9 @@
 ﻿namespace CollegeSchedule_Romankov.Models
 {
-    public class LessonGroupPart
+    public enum LessonGroupPart
     {
+        FULL = 0,
+        SUB1 = 1,
+        SUB2 = 2
     }
 }
