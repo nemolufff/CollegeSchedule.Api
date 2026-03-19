e424e3eab5fedaa5dfa0265e14cb708cbb626656
Matvey <romankovmatvej59@gmail.com>
2026-03-19 08:21:00 +0700

    Add GroupDto

---
+++ CollegeSchedule_Romankov/DTO/GroupDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CollegeSchedule_Romankov.DTO
+{
+    public class GroupDto
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; } = null!;
+    }
+}
